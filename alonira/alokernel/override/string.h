17d24cedba948ee1e068649034349987d98987e7
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2022-12-16 17:26:05 +0000

    Updating to new Genstone backends and builds - everything is very very broken

--- alonira/alokernel/override/string.h
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2022 Emily "TTG" Banerjee <prs.ttg+alonira@pm.me>
-
-#include <gencommon.h>
-
-size_t strnlen(const char* const restrict string, size_t limit);
---
+++ alonira/alonira.mk
@@ -0,0 +1,8 @@
+.PHONY: alonira
+alonira:
+
+.PHONY: test_alonira
+test_alonira:
+
+.PHONY: clean_alonira
+clean_alonira:
